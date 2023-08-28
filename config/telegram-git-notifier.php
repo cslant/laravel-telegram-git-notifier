17ca6ebcfebc1bd5a81791326071fa076eefdc0d
Tan Nguyen <tannp27@gmail.com>
2023-08-28 11:06:29 +0700

    init package

---
+++ config/telegram-git-notifier.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'app' => [
+        'name' => env('APP_NAME', 'Laravel Telegram Git Notify'),
+    ],
+];
---
+++ src/Providers/TelegramGitNotifierServiceProvider.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace LbilTech\LaravelTelegramGitNotifier\App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class TelegramGitNotifierServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot(): void
+    {
+        $configPath = __DIR__.'/../../config/telegram-git-notifier.php';
+        $this->publishes([
+            $configPath => config_path('telegram-git-notifier.php'),
+        ], 'config');
+
+        $routePath = __DIR__.'/../../routes/bot.php';
+        if (file_exists($routePath)) {
+            $this->loadRoutesFrom($routePath);
+        }
+
+        $helperPath = __DIR__.'/../../common/helpers.php';
+        if (file_exists($helperPath)) {
+            require_once $helperPath;
+        }
+
+        $this->loadTranslationsFrom(__DIR__.'/../../lang', 'telegram-git-notifier');
+
+        $this->publishes([
+            __DIR__.'/../../lang' => resource_path('lang/vendor/laravel-generator'),
+        ], 'lang');
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register(): void
+    {
+        $configPath = __DIR__.'/../../config/telegram-git-notifier.php';
+        $this->mergeConfigFrom($configPath, 'telegram-git-notifier');
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array|null
+     */
+    public function provides(): ?array
+    {
+        return ['telegram-git-notifier'];
+    }
+}
