39c01ea4e7c5cc9a369b2d71c9c4346fb8defce1
Xuan Thinh <pxthinh.vn@gmail.com>
2023-10-29 19:54:21 +0700

    update config

--- config/telegram-git-notifier.php
+++ config/telegram-git-notifier.php
@@ -10,31 +10,55 @@ return [
     'bot' => [
         'token'           => env('TELEGRAM_BOT_TOKEN', ''),
         'chat_id'         => env('TELEGRAM_BOT_CHAT_ID', ''),
-        'notify_chat_ids' => explode(
-            ',',
-            env('TELEGRAM_NOTIFY_CHAT_IDS', '')
-        ),
+        'notify_chat_ids' => env('TELEGRAM_NOTIFY_CHAT_IDS', '')
     ],
 
     'author' => [
-        'discussion'     => env('TGN_AUTHOR_CONTACT', 'https://t.me/tannp27'),
+        'discussion'  => env('TGN_AUTHOR_CONTACT', 'https://t.me/tannp27'),
         'source_code' => env(
             'TGN_AUTHOR_SOURCE_CODE',
             'https://github.com/lbiltech/laravel-telegram-git-notifier'
         ),
     ],
 
+    'data_file' => [
+        'setting' => env(
+            'TGN_PATH_SETTING',
+            storage_path('/app/tgn-json/tgn-settings.json')
+        ),
+
+        'platform' => [
+            'gitlab' => env(
+                'TGN_PATH_PLATFORM_GITLAB',
+                storage_path('/app/tgn-json/gitlab-events.json')
+            ),
+            'github' => env(
+                'TGN_PATH_PLATFORM_GITHUB',
+                storage_path('/app/tgn-json/github-events.json')
+            ),
+        ],
+    ],
+
     'view' => [
-        'event'   => [
+        'default' => env(
+            'TGN_VIEW_DEFAULT',
+            base_path('resources/views/vendor/laravel-telegram-git-notify')
+        ),
+
+        'path' => env('TGN_VIEW_PATH', 'resources/views'),
+
+        'event' => [
             'default' => env('TGN_VIEW_EVENT_DEFAULT', 'default'),
         ],
+
         'globals' => [
             'access_denied' => env(
                 'TGN_VIEW_GLOBALS_ACCESS_DENIED',
                 'globals.access_denied'
             ),
         ],
-        'tools'   => [
+
+        'tools' => [
             'settings'            => env(
                 'TGN_VIEW_TOOL_SETTING',
                 'tools.settings'
@@ -53,4 +77,5 @@ return [
             ),
         ],
     ],
+
 ];
