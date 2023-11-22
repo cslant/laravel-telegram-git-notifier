b331be352051155c2e75e35c511cd0d6a488d55d
Tan Nguyen <tannp27@gmail.com>
2023-11-22 15:50:57 +0700

    feat: update multi lang

---
+++ lang/de/events/github/push.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'default' => [
+        'title' => '👷⚙️ <b>:count</b> new :noun to 🦑<b>:user:<code>:branch</code></b>',
+        'commit' => ':commit: :commit_message - by <i>:commit_name</i>',
+        'pushed' => '👤 Pushed by : <b>:name</b>',
+    ],
+];
