5e37b8e79d2f096283af551cb8b517553991317a
Tan Nguyen <tannp27@gmail.com>
2023-11-23 11:25:50 +0700

    fix: update issues events for gitlab

--- resources/views/events/gitlab/issue/update.blade.php
+++ resources/views/events/gitlab/issue/update.blade.php
@@ -14,4 +14,13 @@
 📢 <b>{{ $payload->object_attributes->title }}</b>
 
 @include('tg-notifier::events.shared.partials.gitlab._assignees', compact('payload', 'event'))
-@include('tg-notifier::events.shared.partials.gitlab._body', compact('payload', 'event'))
+
+@if(isset($payload->changes->title))
+{!! __('tg-notifier::events/gitlab/issues.edited.changes.title.name') !!}
+    {!! __('tg-notifier::events/gitlab/issues.edited.changes.title.from', ['title_from' => $payload->changes->title->previous]) !!}
+    {!! __('tg-notifier::events/gitlab/issues.edited.changes.title.to', ['title_to' => $payload->changes->title->current]) !!}
+@endif
+@if(isset($payload->changes->description))
+{!! __('tg-notifier::events/gitlab/issues.edited.changes.body.title') !!}
+    {!! __('tg-notifier::events/gitlab/issues.edited.changes.body.message') !!}
+@endif
