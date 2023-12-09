ed8fb21ff8a384388391e17fe4b7f6fe837dc83d
Tan Nguyen <tannp27@gmail.com>
2023-12-10 02:22:40 +0700

    reformat styles for the config file

--- config/telegram-git-notifier.php
+++ config/telegram-git-notifier.php
@@ -7,27 +7,24 @@ $configFileStorageFolder = storage_path(
 return [
     'defaults' => [
         'paths' => [
-            'views' => env(
-                'TGN_DEFAULT_PATH_VIEW',
-                base_path('resources/views/vendor/tg-notifier')
-            ),
+            'views' => env('TGN_DEFAULT_PATH_VIEW', base_path('resources/views/vendor/tg-notifier')),
         ],
 
-        // Set route prefix for telegram git notifier app
+        /* Set route prefix for telegram git notifier app */
         'route_prefix' => env('TGN_DEFAULT_ROUTE_PREFIX', 'telegram-git-notifier'),
     ],
 
     'app' => [
-        'name'     => env('TGN_APP_NAME', 'Laravel Telegram Git Notifier'),
+        'name' => env('TGN_APP_NAME', 'Laravel Telegram Git Notifier'),
 
-        // Required for the bot to work properly
-        'url'      => env('TGN_APP_URL', 'http://localhost:8000/telegram-git-notifier'),
+        /* Required for the bot to work properly */
+        'url' => env('TGN_APP_URL', 'http://localhost:8000/telegram-git-notifier'),
         'timezone' => env('TIMEZONE', 'Asia/Ho_Chi_Minh'),
     ],
 
     'bot' => [
-        'token'           => env('TELEGRAM_BOT_TOKEN', ''),
-        'chat_id'         => env('TELEGRAM_BOT_CHAT_ID', ''),
+        'token' => env('TELEGRAM_BOT_TOKEN', ''),
+        'chat_id' => env('TELEGRAM_BOT_CHAT_ID', ''),
 
         /**
          * Set the chat IDs that will receive notifications
@@ -45,77 +42,39 @@ return [
     ],
 
     'author' => [
-        'discussion'  => env(
-            'TGN_AUTHOR_DISCUSSION',
-            'https://github.com/cslant/laravel-telegram-git-notifier/discussions'
-        ),
-        'source_code' => env(
-            'TGN_AUTHOR_SOURCE_CODE',
-            'https://github.com/cslant/laravel-telegram-git-notifier'
-        ),
+        'discussion' => env('TGN_AUTHOR_DISCUSSION', 'https://github.com/cslant/laravel-telegram-git-notifier/discussions'),
+        'source_code' => env('TGN_AUTHOR_SOURCE_CODE', 'https://github.com/cslant/laravel-telegram-git-notifier'),
     ],
 
-    /** Set the path to the data file */
+    /* Set the path to the data file */
     'data_file' => [
         'storage_folder' => $configFileStorageFolder,
-
-        'setting' => env(
-            'TGN_PATH_SETTING',
-            $configFileStorageFolder.'/tgn-settings.json'
-        ),
+        'setting' => env('TGN_PATH_SETTING', $configFileStorageFolder.'/tgn-settings.json'),
 
         'platform' => [
-            'gitlab' => env(
-                'TGN_PATH_PLATFORM_GITLAB',
-                $configFileStorageFolder.'/gitlab-events.json'
-            ),
-            'github' => env(
-                'TGN_PATH_PLATFORM_GITHUB',
-                $configFileStorageFolder.'/github-events.json'
-            ),
+            'gitlab' => env('TGN_PATH_PLATFORM_GITLAB', $configFileStorageFolder.'/gitlab-events.json'),
+            'github' => env('TGN_PATH_PLATFORM_GITHUB', $configFileStorageFolder.'/github-events.json'),
         ],
     ],
 
-    /** Set the path to the view file */
+    /* Set the path to the view file */
     'view' => [
         'ignore-message' => env('IGNORE_MESSAGE', 'ignore-message'),
-
         'namespace' => env('TGN_VIEW_NAMESPACE', 'tg-notifier'),
-
-        'default' => env(
-            'TGN_VIEW_DEFAULT',
-            base_path('resources/views/vendor/tg-notifier')
-        ),
-
+        'default' => env('TGN_VIEW_DEFAULT', base_path('resources/views/vendor/tg-notifier')),
         'event' => [
             'default' => env('TGN_VIEW_EVENT_DEFAULT', 'default'),
         ],
 
         'globals' => [
-            'access_denied' => env(
-                'TGN_VIEW_GLOBALS_ACCESS_DENIED',
-                'globals.access_denied'
-            ),
+            'access_denied' => env('TGN_VIEW_GLOBALS_ACCESS_DENIED', 'globals.access_denied'),
         ],
 
         'tools' => [
-            'settings'            => env(
-                'TGN_VIEW_TOOL_SETTING',
-                'tools.settings'
-            ),
-            'custom_event_action' => env(
-                'TGN_VIEW_TOOL_CUSTOM_EVENT_ACTION',
-                'tools.custom_event_action'
-            ),
-            'custom_event'        => env(
-                'TGN_VIEW_TOOL_CUSTOM_EVENT',
-                'tools.custom_event'
-            ),
-            'set_menu_cmd'        => env(
-                'TGN_VIEW_TOOL_SET_MENU_COMMAND',
-                'tools.set_menu_cmd'
-            ),
+            'settings' => env('TGN_VIEW_TOOL_SETTING', 'tools.settings'),
+            'custom_event_action' => env('TGN_VIEW_TOOL_CUSTOM_EVENT_ACTION', 'tools.custom_event_action'),
+            'custom_event' => env('TGN_VIEW_TOOL_CUSTOM_EVENT', 'tools.custom_event'),
+            'set_menu_cmd' => env('TGN_VIEW_TOOL_SET_MENU_COMMAND', 'tools.set_menu_cmd'),
         ],
     ],
-
 ];
