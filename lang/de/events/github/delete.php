215fae484c04a305a2b214a65956d1ad836ba456
Avaelo <127422332+avaelo@users.noreply.github.com>
2024-02-07 20:15:56 +0700

    add multi lang for delete event

---
+++ lang/de/events/github/delete.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'title' => '🗑 <b>Webhook gelöscht</b>',
+    'organization' => '🏢 Organisation: <b>:organization</b>',
+    'full_name' => '📦 Repository: 🦑<b>:full_name</b>',
+    'sender' => '👤 Sender (der das Ereignis ausgelöst hat): <b>:sender</b>',
+];
---
+++ lang/vi/events/github/delete.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'title' => '🗑 <b>Webhook Deleted</b>',
+    'organization' => '🏢 Organization: <b>:organization</b>',
+    'full_name' => '📦 Repository: 🦑<b>:full_name</b>',
+    'sender' => '👤 Sender (triggered the event): <b>:sender</b>',
+];
