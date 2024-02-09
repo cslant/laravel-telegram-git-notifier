419a41a60a0de424b26f301d310063b7ab00ecb3
kenautomus <kenautomus@users.noreply.github.com>
2024-02-09 14:44:39 +0700

    create star action template

---
+++ lang/en/events/github/star.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'created' => [
+        'title' => '🎉 <b>New Star 🌟</b> from 🦑:user',
+        'seeder' => '👤 Starer: <b>:seeder_login</b> 👀',
+    ],
+
+    'deleted' => [
+        'title' => '🚫 <b>Star 🌟 Removed</b> from 🦑:user',
+        'remover' => '👤 Remover: <b>:seeder_login</b> 👀',
+    ],
+];
--- resources/views/events/github/meta/deleted.blade.php
+++ resources/views/events/github/meta/deleted.blade.php
@@ -8,11 +8,11 @@
 {!! __('tg-notifier::events/github/meta.delete.title') !!}
 
 @if(isset($payload->organization))
-    {!! __('tg-notifier::events/github/meta.delete.organization', ['organization' => $payload->organization->login]) !!}
+{!! __('tg-notifier::events/github/meta.delete.organization', ['organization' => $payload->organization->login]) !!}
 @endif
 @if(isset($payload->repository))
-    {!! __('tg-notifier::events/github/meta.delete.full_name', ['full_name' => $payload->repository->full_name]) !!}
+{!! __('tg-notifier::events/github/meta.delete.full_name', ['full_name' => $payload->repository->full_name]) !!}
 @endif
 @if(isset($payload->sender))
-    {!! __('tg-notifier::events/github/meta.delete.sender', ['sender' => $payload->sender->login]) !!}
+{!! __('tg-notifier::events/github/meta.delete.sender', ['sender' => $payload->sender->login]) !!}
 @endif
