aeb303fb94941eb5d75e8d488bfc410df7ae2f61
Tan Nguyen <tannp27@gmail.com>
2024-05-05 04:06:12 +0700

    fix: update layout for workflow run actions

--- lang/de/events/github/workflow_run.php
+++ lang/de/events/github/workflow_run.php
@@ -4,24 +4,25 @@ return [
     'completed' => [
         'success' => [
             'title' => '🎉 <b>Workflow Completed</b> from 🦑 :repo',
-            'body' => 'Done workflow: 🎉 <b>:name</b> ✨ ',
+            'body' => 'Done workflow: 🎉 <b><code>:name</code></b> ✨ ',
         ],
         'failure' => [
             'title' => '🚫 <b>Workflow Failed</b> from 🦑:repo',
-            'body' => 'Failed workflow: 🚫 <b>:name</b> ❌',
+            'body' => 'Failed workflow: 🚫 <b><code>:name</code></b> ❌',
         ],
         'cancelled' => [
             'title' => '❌ <b>Workflow Cancelled</b> from 🦑 :repo',
-            'body' => 'Cancelled workflow: 🚨 <b>:name</b> ❌ ',
+            'body' => 'Cancelled workflow: 🚨 <b><code>:name</code></b> ❌ ',
         ],
         'default' => [
             'title' => "🚨 <b>Workflow Can't Success</b> from 🦑:repo",
-            'body' => "Can't Success workflow: 🚨 <b>:name</b> ❌",
+            'body' => "Can't Success workflow: 🚨 <b><code>:name</code></b> ❌",
         ],
     ],
     'requested' => [
         'title' => ' <b>Workflow Requested</b> from 🦑:repo',
-        'body' => 'Running workflow: 💥 <b>:name</b> ⏳',
+        'body' => 'Running workflow: 💥 <b><code>:name</code></b> ⏳',
     ],
-    'link' => '🔗 Workflow run link: :link',
+    'link' => 'Workflow run link: :link',
+    'display_title' => 'Title',
 ];
--- resources/views/events/github/workflow_run/requested.blade.php
+++ resources/views/events/github/workflow_run/requested.blade.php
@@ -7,5 +7,5 @@
 {!! __('tg-notifier::events/github/workflow_run.requested.title', ['repo' => "<a href='{$payload->repository->html_url}'>{$payload->repository->full_name}</a>"]) !!}
 
 {!! __('tg-notifier::events/github/workflow_run.requested.body', ['name' => $payload->workflow_run->name]) !!}
-
+📝 {!! __('tg-notifier::events/github/workflow_run.display_title') !!}: <code>{!! $payload->workflow_run->display_title !!}</code>
 {!! __('tg-notifier::events/github/workflow_run.link', ['link' => "<a href='{$payload->workflow_run->html_url}'>{$payload->workflow_run->event} - {$payload->workflow_run->name}</a>"]) !!}
