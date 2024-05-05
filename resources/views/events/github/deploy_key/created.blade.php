43a4cf6b3f6cc3c7eb8eded85b2011eee0502596
Tan Nguyen <tannp27@gmail.com>
2024-05-06 00:17:10 +0700

    fix: update view template for deploy key event

--- resources/views/events/github/deploy_key/created.blade.php
+++ resources/views/events/github/deploy_key/created.blade.php
@@ -2,13 +2,14 @@
 /**
  * @var $payload object
  */
+
 ?>
 
-{!! __('tg-notifier::events/github/deploy_key.created.title', [
-    'repo' => "<a href='{$payload->repository->html_url}'>{$payload->repository->full_name}</a>",
-    'user' => "<b>{$payload->key->added_by}</b>",
-        ]
-    ) !!}
+💬 {!! __('tg-notifier::events/github/deploy_key.created.title', [
+        'repo' => "🦑 <a href='{$payload->repository->html_url}'>{$payload->repository->full_name}</a>",
+        'user' => "<b>{$payload->key->added_by}</b>",
+    ]
+) !!}
 
-📢 <b>{{ $payload->key->title }}</b>
+📢 {!! __('tg-notifier::events/github/deploy_key.key_title') !!}: <code>{{ $payload->key->title }}</code>
 {!! __('tg-notifier::events/github/deploy_key.created.message') !!}
