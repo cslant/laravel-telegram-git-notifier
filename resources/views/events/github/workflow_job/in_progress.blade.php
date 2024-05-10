e5af940b82a52a83e2c00dfbe813dfd7688826c7
Tan Nguyen <tannp27@gmail.com>
2024-05-11 04:11:27 +0700

    fix: update view template for workflow job actions

--- resources/views/events/github/workflow_job/in_progress.blade.php
+++ resources/views/events/github/workflow_job/in_progress.blade.php
@@ -7,5 +7,5 @@
 
 🔧 {!! __('tg-notifier::events/github/workflow_job.in_progress.progress', ['repo' => "🦑<a href='{$payload->repository->html_url}'>{$payload->repository->html_url}</a>"]) !!}
 
-{!! __('tg-notifier::events/github/workflow_job.in_progress.running') !!}: 💥 <code>{{ $payload->workflow_job->workflow_name }}</code> ⏳
-🔗 {!! __('tg-notifier::events/github/workflow_job.link', ['link' => "<a href='{$payload->workflow_job->html_url}'>{$payload->workflow_job->name}</a>"]) !!}
+🚀 {!! __('tg-notifier::events/github/workflow_job.in_progress.running') !!}: 💥 <code>{{ $payload->workflow_job->name }}</code> ⏳
+🔗 {!! __('tg-notifier::events/github/workflow_job.link', ['link' => "<a href='{$payload->workflow_job->html_url}'>{$payload->workflow_job->workflow_name}</a>"]) !!}
--- resources/views/events/github/workflow_job/queued.blade.php
+++ resources/views/events/github/workflow_job/queued.blade.php
@@ -7,6 +7,5 @@
 
 {!! __('tg-notifier::events/github/workflow_job.queued.title', ['repo' => "🦑<a href='{$payload->repository->html_url}'>{$payload->repository->full_name}</a>"]) !!}
 
-{!! __('tg-notifier::events/github/workflow_job.queued.name') !!}: 💥 <code>{{ $payload->workflow_job->workflow_name }}</code> ⏰
-
-🔗 {!! __('tg-notifier::events/github/workflow_job.link', ['link' => "<a href='{$payload->workflow_job->html_url}'>{$payload->workflow_job->name}</a>"]) !!}
+🚀 {!! __('tg-notifier::events/github/workflow_job.queued.name') !!}: 💥 <code>{{ $payload->workflow_job->name }}</code> ⏰
+🔗 {!! __('tg-notifier::events/github/workflow_job.link', ['link' => "<a href='{$payload->workflow_job->html_url}'>{$payload->workflow_job->workflow_name}</a>"]) !!}
