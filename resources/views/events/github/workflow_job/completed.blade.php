c8d8fb27656cf5cc5a96bd5e509c7beaca3eb157
Tan Nguyen <tannp27@gmail.com>
2024-05-11 04:11:31 +0700

    feat: show status and run time for workflow job complete

--- resources/views/events/github/workflow_job/completed.blade.php
+++ resources/views/events/github/workflow_job/completed.blade.php
@@ -12,10 +12,15 @@ if ($payload->workflow_job->conclusion === 'success') {
     $icon = '🚫';
     $last = '❌';
 }
+
+$startedAt = new DateTime($payload->workflow_job->started_at);
+$completedAt = new DateTime($payload->workflow_job->completed_at);
+$interval = $completedAt->diff($startedAt);
+$allSeconds = $interval->s + $interval->i * 60 + $interval->h * 3600;
 ?>
 
 {{ $icon }} {!! __("tg-notifier::events/github/workflow_job.completed.$status", ['repo' => "🦑<a href='{$payload->repository->html_url}'>{$payload->repository->full_name}</a>"]) !!}
 
 🚀 {!! __('tg-notifier::events/github/workflow_job.name') !!}: {{ $icon }} <code>{{ $payload->workflow_job->name }}</code> {{ $last }}
-🚨 {!! __('tg-notifier::events/github/workflow_job.status.title') !!}: {!! __('tg-notifier::events/github/workflow_job.status.'.$status) !!}
+🚨 {!! __('tg-notifier::events/github/workflow_job.status.title') !!}: <code>{!! __('tg-notifier::events/github/workflow_job.status.'.$status) !!}</code> 🕓 <code>{{ $allSeconds }}s</code>
 🔗 {!! __('tg-notifier::events/github/workflow_job.link', ['link' => "<a href='{$payload->workflow_job->html_url}'>{$payload->workflow_job->workflow_name}</a>"]) !!}
