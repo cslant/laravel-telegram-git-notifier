4ee11a7376f0b129c2929a7a63ff8c9f0bd4dc1d
Tan Nguyen <tannp27@gmail.com>
2024-05-20 23:11:27 +0700

    fix: translations for tag push event

--- lang/en/events/gitlab/tag_push.php
+++ lang/en/events/gitlab/tag_push.php
@@ -2,6 +2,6 @@
 
 return [
     'title' => '<b>A new tag has been pushed to the project</b> :repo',
-    'name' => '<b>Tag Name</b>: :tag_name',
+    'name' => 'Tag Name',
     'pusher' => ' Pushed by',
 ];
