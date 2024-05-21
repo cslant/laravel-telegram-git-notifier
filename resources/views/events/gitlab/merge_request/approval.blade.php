155a2f4fdbd1934b7ce2bfc54eb18b89ebb80611
Tan Nguyen <tannp27@gmail.com>
2024-05-21 20:01:55 +0700

    fix: update gitlab merge request view templates

--- resources/views/events/gitlab/merge_request/approval.blade.php
+++ resources/views/events/gitlab/merge_request/approval.blade.php
@@ -7,12 +7,12 @@
 ?>
 👍 {!! __('tg-notifier::events/gitlab/merge_request.approval.title', [
         'repo' => "✅ - 🦊<a href='{$payload->object_attributes->url}'>{$payload->project->path_with_namespace}#{$payload->object_attributes->iid}</a>",
-        'user' => "<b>{$payload->user->name}</b>"
+        'user' => "<code>{$payload->user->name}</code>"
     ]) !!}
 
 🛠 <b>{{ $payload->object_attributes->title }}</b>
 
-🌳 {{ $payload->object_attributes->source_branch }} -> {{ $payload->object_attributes->target_branch }} 🎯
+🌳 {{ __('tg-notifier::app.branch') }}: {{ $payload->object_attributes->source_branch }} -> {{ $payload->object_attributes->target_branch }} 🎯
 @include('tg-notifier::events.shared.partials.gitlab._assignees', compact('payload', 'event'))
 @include('tg-notifier::events.gitlab.merge_request.partials._reviewers', compact('payload'))
 @include('tg-notifier::events.shared.partials.gitlab._body', compact('payload', 'event'))
--- resources/views/events/gitlab/merge_request/open.blade.php
+++ resources/views/events/gitlab/merge_request/open.blade.php
@@ -12,7 +12,7 @@
 
 🛠 <b>{{ $payload->object_attributes->title }}</b>
 
-🌳 {{ $payload->object_attributes->source_branch }} -> {{ $payload->object_attributes->target_branch }} 🎯
+🌳 {{ __('tg-notifier::app.branch') }}: {{ $payload->object_attributes->source_branch }} -> {{ $payload->object_attributes->target_branch }} 🎯
 @include('tg-notifier::events.shared.partials.gitlab._assignees', compact('payload', 'event'))
 @include('tg-notifier::events.gitlab.merge_request.partials._reviewers', compact('payload'))
 @include('tg-notifier::events.shared.partials.gitlab._body', compact('payload', 'event'))
