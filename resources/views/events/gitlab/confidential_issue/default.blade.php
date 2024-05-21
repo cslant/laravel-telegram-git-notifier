b8cde2be5ed2e4f16926802a71795aa197a52f93
Tan Nguyen <tannp27@gmail.com>
2024-05-21 23:17:58 +0700

    feat: add gitlab default issue, confidential issue and merge request for test webhook

---
+++ resources/views/events/gitlab/confidential_issue/default.blade.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @var $payload object
+ * @var $event string
+ */
+
+?>
+🔥 {!! __('tg-notifier::events/gitlab/issues.confidential_issue', [
+        'issue' => "🦊<a href='{$payload->object_attributes->url}'>{$payload->project->path_with_namespace}#{$payload->object_attributes->id}</a>",
+        'user' => "<b>{$payload->user->name}</b>"
+    ]
+) !!}
+
+📢 {{ __('tg-notifier::app.title') }}: <code>{{ $payload->object_attributes->title }}</code>
+📡 {{ __('tg-notifier::app.state') }}: <code>{{ $payload->object_attributes->state }}</code>
+🔒 {{ __('tg-notifier::app.confidential') }}: <code>{{ $payload->object_attributes->confidential }}</code>
+@include('tg-notifier::events.shared.partials.gitlab._assignees', compact('payload', 'event'))
+@include('tg-notifier::events.shared.partials.gitlab._body', compact('payload', 'event'))
---
+++ resources/views/events/gitlab/merge_request/default.blade.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @var $payload object
+ * @var $event string
+ */
+
+?>
+🔥 {{ __('tg-notifier::events/gitlab/merge_request.default') }}
+
+🛠 <b>{{ $payload->object_attributes->title }}</b>
+🌳 {{ __('tg-notifier::app.branch') }}: {{ $payload->object_attributes->source_branch }} -> {{ $payload->object_attributes->target_branch }} 🎯
+@include('tg-notifier::events.shared.partials.gitlab._assignees', compact('payload', 'event'))
+@include('tg-notifier::events.gitlab.merge_request.partials._reviewers', compact('payload'))
+@include('tg-notifier::events.shared.partials.gitlab._body', compact('payload', 'event'))
